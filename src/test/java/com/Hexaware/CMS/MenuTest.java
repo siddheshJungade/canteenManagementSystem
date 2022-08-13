687b1e0550b18ce8800f8734e2f24182083b2508
siddhesh jungade <connect@siddheshjungade.me>
2022-08-13 08:17:38 +0530

    git workflow added .github

--- src/test/java/com/Hexaware/CMS/MenuTest.java
+++ src/test/java/com/Hexaware/CMS/MenuTest.java
@@ -13,31 +13,4 @@ import org.junit.Test;
  */
 public class MenuTest 
 {
-    static Menu menu;
-    @BeforeClass
-    public static void beforeClass(){
-        menu=new Menu(201,"Veggie Farmhouse Pizza",320,001);
-    }
-   
-    @Test
-    public  void testGet(){
-       assertEquals(201,menu.getFoodId());
-       assertEquals("Veggie Farmhouse Pizza",menu.getFoodName());
-       assertEquals(320,menu.getFoodPrice()); 
-    }
-
-    @Test
-    public void testToString(){
-        String str=menu.toString();
-        String expected = 
-            "food id:"+menu.getFoodId()+
-            "food Name:"+menu.getFoodName()+
-            "food Price"+menu.getFoodPrice() + 
-            "Vendor id :"+menu.getVendorId();
-        assertEquals(expected,str);
-    }
-    @AfterClass
-    public static void afterClass(){
-        menu=null;
-    }
 }
